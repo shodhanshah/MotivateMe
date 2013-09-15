a4a4337ebd25a8c5abadd48d7578c0f9aed17bb7
Shodhan Shah <shodhan2001@new-host-3.home>
2013-09-15 19:27:05 -0400

    Initial Commit

---
+++ GoodMorning/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GoodMorning
+//
+//  Created by Jatin Shah on 9/15/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GoodMorning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoodMorning
+//
+//  Created by Jatin Shah on 9/15/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
